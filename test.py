2de6a17363a78d8def26e99077536415caa832a8
vladv <vlad_gabriel.vlaicu@stud.acs.upb.ro>
2023-12-22 11:11:43 +0200

    asd

--- test.py
+++ test.py
@@ -5,9 +5,12 @@ def test(net):
     net.get("h1").sendCmd("python3 -m http.server 9000 &")
     
     # Start the client
-    for i in range(100):
+    for i in range(500):
         print(i)
         net.get("c1").sendCmd("python3 client.py -p http 10.10.101.2:9000 &")
+        output = net.get("c1").waitOutput()
+        print("Client output:")
+        print(output)
     # Wait for the client command to finish and retrieve its output
     output = net.get("c1").waitOutput()
     print("Client output:")
